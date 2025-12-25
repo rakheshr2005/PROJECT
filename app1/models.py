f09a7f6aee13becc6db85f4889e521fcbde9ddc3
Rakhesh R <rrakhesh2005@gmail.com>
2025-12-25 23:09:15 +0530

    added model user_deatils

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class user_details(models.Model):
+    firstname=models.CharField(max_length=100)
+    lastname=models.CharField(max_length=100)
+    dob=models.DateField()
+    email=models.EmailField(max_length=50)
+    password=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.firstname
+
