aed43fc3d96212dc73473d3e5f351389b36aa86d
Rakhesh R <rrakhesh2005@gmail.com>
2025-12-27 19:31:44 +0530

    added labels for input types

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import user_details
+
 
 # Create your views here.
 
@@ -8,3 +10,20 @@ def index(request):
 def login(request):
         return render(request,'app1/login.html')
 
+def register_user(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        dob = request.POST.get('dob')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Save data to database
+        user = user_details(
+            firstname=firstname,
+            lastname=lastname,
+            dob=dob,
+            email=email,
+            password=password
+        )
+        user.save()
